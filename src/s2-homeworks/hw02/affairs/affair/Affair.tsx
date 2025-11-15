7a910222f0c0e0680097ff5e1e7a801e5490af2d
SemenKr <samkrekotyn@gmail.com>
2025-11-15 10:34:28 +0300

    fix bags

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -5,13 +5,14 @@ import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
+    id: number
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+       props.deleteAffairCallback(props.id)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -26,6 +27,7 @@ function Affair(props: AffairPropsType) {
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
 
+                {props.affair.name}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
@@ -37,6 +39,7 @@ function Affair(props: AffairPropsType) {
                 className={buttonClass}
                 // need to fix
 
+                onClick={deleteCallback}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
