b7f03b88fe2d89405dc9c4052ea7ddbcb862e9d4
Your Name <you@example.com>
2026-01-25 11:24:49 +0300

    hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -22,11 +22,22 @@ const SuperRange: React.FC<SliderProps> = (props) => {
                 },
 
                 '& .MuiSlider-thumb': {
-                    width: 14,
-                    height: 14,
+                    width: 18,
+                    height: 18,
                     backgroundColor: '#fff',
-                    border: '2px solid #00cc22',
+                    border: '1px solid #00cc22',
                     boxShadow: 'none',
+                    '&::before': {
+                        content: '""',
+                        width: 6,
+                        height: 6,
+                        backgroundColor: '#00cc22',
+                        borderRadius: '50%',
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                    },
 
                     '&:hover, &.Mui-focusVisible': {
                         boxShadow: '0 0 0 6px rgba(0, 204, 34, 0.15)',
